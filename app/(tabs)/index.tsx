5436e2fd6e165f633a4a61267a24a87a86bece46
NiceZGT730 <ngamemax@gmail.com>
2025-12-04 00:03:52 +0700

    edit name MovieFlix and create Tabs menu

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native";
-import "./global.css";
+
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-extralight text-primary">
+      <Text className="text-3xl font-extralight text-dark-200">
         {" "}
         Welcome to My Movie App!
       </Text>
