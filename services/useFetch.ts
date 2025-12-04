27443af53815badf0810c9a1b4377e239f8b0e57
NiceZGT730 <ngamemax@gmail.com>
2025-12-04 17:59:00 +0700

    Fetch api success show only title index.tsx

---
+++ services/useFetch.ts
@@ -0,0 +1,46 @@
+//fetchMovie
+//fetchMovieDetails
+
+import { useEffect, useState } from "react";
+
+//useFetch(FetchMovie)
+const useFetch = <T>(
+  fetchFunction: () => Promise<T>,
+  autoFetch: boolean = true
+) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const result = await fetchFunction();
+
+      setData(result);
+    } catch (err) {
+      // @ts-ignore
+      if (err instanceof Error ? err : new Error("An error occurred"));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, refetch: fetchData, reset };
+};
+
+export default useFetch;
